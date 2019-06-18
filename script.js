f969d85311f383e1ce32264814aba555aede12b0
ZelleM <groovyrlm@gmail.com>
2019-06-18 17:52:36 -0400

    Cash Register sound works

--- script.js
+++ script.js
@@ -7,31 +7,46 @@
 //declare functions as page loads to make sure that the elements are shown and hidden
 $(document).ready(function () {    
     
+    //Declare needed variables, initiate numberSplittingCheck to 1
     var billTotal;
     var tipPercentage;
     var tipAmount;
     var grandTotal;
-    var numberSplittingCheck;
+    var numberSplittingCheck=1;
     var amountEach;
     
-    //on button click calculate the tip
-const myButton = document.querySelector("#calculate"); 
+    //on button click do the math and display the results
+    const myButton = document.querySelector("#calculate"); 
 
-myButton.addEventListener('click', () => {    // calculate the numbers
+    myButton.addEventListener('click', () => {    // calculate the numbers
         billTotal=document.getElementById("billTotal-input").value;
         tipPercentage=document.getElementById("tipPercentage-input").value;
         tipAmount=parseFloat((tipPercentage - 1) * billTotal).toFixed(2);
         grandTotal = parseFloat(billTotal * tipPercentage).toFixed(2);
         numberSplittingCheck=document.getElementById("numberSplittingCheck-input").value;
         amountEach = parseFloat(grandTotal / numberSplittingCheck).toFixed(2);
+        validateData();
         //write back to the page
         document.querySelector("#totalBill").innerHTML="<br>" + "The Bill is $" + "<strong>" + billTotal + "</strong>";
         document.querySelector("#tipAmount").innerHTML="The Total Tip is $" + "<strong>" + tipAmount + "</strong>";
         document.querySelector("#totalTotal").innerHTML="Making the Grand Total " + "<strong>" + "$" + grandTotal + "</strong>";
         document.querySelector("#numberSplittingCheck").innerHTML="Which, split by " + "<strong>" + numberSplittingCheck + "</strong>" + " people";
         document.querySelector("#amountEach").innerHTML="is $" + "<strong>" + amountEach + "</strong>" + " per person";
-
+ 
     }) //end myButton
+
+    function validateData(){
+        if (billTotal == "" || billTotal.includes("-")) { //if billtotal is blank or has a negative number
+          alert("Please enter a positive number as the bill total");
+          return false;
+        } else if (numberSplittingCheck == "" || numberSplittingCheck <= 0) { //default to 1 if blank or less than 1
+          numberSplittingCheck=1;
+          return;
+        } //end if/else
+    } // end validate function
+
+        var audio = new Audio('Cash Register Cha Ching-SoundBible.mp3'); // identify audio file
+        $('#calculate').click( () => audio.play() ); // play it when button is clicked
 
 }) // end document ready
 
