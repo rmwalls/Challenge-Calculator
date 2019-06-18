8bb29db09f2e2425c4dd40c639245aace1ed00c6
ZelleM <groovyrlm@gmail.com>
2019-06-17 21:38:36 -0400

    Essentially works, needs some validation

--- script.js
+++ script.js
@@ -1,50 +1,36 @@
-// allow person to enter 
-//      the amount of the bill
-//      tip percentage they want to add --- use a drop down with choices
-//      number of people to split the bill by
-// n
-// then multiply the bill by the tip
-// divide that total  by the number to split (whole number only)
-// so bill amount *  1.tip% / number = amount each
+// need to 
+//      validate input
+//      default number of people to split the bill by if not entered
+// 
 
-
-$(document).ready(function () {
-    //declare functions as page loads to make sure that the elements are shown and hidden
+//declare functions as page loads to make sure that the elements are shown and hidden
+$(document).ready(function () {    
     
-    
-    var billWithTip=0;
-    var numberSplittingCheck=0;
-    var amountEach=0
+    var billTotal;
+    var tipPercentage;
+    var tipAmount;
+    var grandTotal;
+    var numberSplittingCheck;
+    var amountEach;
     
     //on button click calculate the tip
-    document.getElementById("calculate").onclick = calculateTip();
+const myButton = document.querySelector("#calculate"); 
 
-    function calculateTip() {
-        //store values
-        var billTotal=getElementById("billTotal-input").value;
-        console.log("bill total= " + billTotal);
-        var tipPercentage=document.getElementById("tipPercentage-input").value;
-        console.log("tip % = " + tipPercentage);
-        
-    } //end calculate tip
+myButton.addEventListener('click', () => {    // calculate the numbers
+        billTotal=document.getElementById("billTotal-input").value;
+        tipPercentage=document.getElementById("tipPercentage-input").value;
+        tipAmount=parseFloat((tipPercentage - 1) * billTotal).toFixed(2);
+        grandTotal = parseFloat(billTotal * tipPercentage).toFixed(2);
+        numberSplittingCheck=document.getElementById("numberSplittingCheck-input").value;
+        amountEach = parseFloat(grandTotal / numberSplittingCheck).toFixed(2);
+        //write back to the page
+        document.querySelector("#totalBill").innerHTML="<br>" + "The Bill is $" + "<strong>" + billTotal + "</strong>";
+        document.querySelector("#tipAmount").innerHTML="The Total Tip is $" + "<strong>" + tipAmount + "</strong>";
+        document.querySelector("#totalTotal").innerHTML="Making the Grand Total " + "<strong>" + "$" + grandTotal + "</strong>";
+        document.querySelector("#numberSplittingCheck").innerHTML="Which, split by " + "<strong>" + numberSplittingCheck + "</strong>" + " people";
+        document.querySelector("#amountEach").innerHTML="is $" + "<strong>" + amountEach + "</strong>" + " per person";
 
-    function calculateGrandTotal() {
-
-    }
-
-    function amountEach() {
-
-    }
-
-    /*on page load
-    function load() {
-        $("#area").hide("fast");
-        $("#carouselTattoo").hide("fast");
-        $("#imageParagraph").hide("fast");
-        $("#lastInput").hide("fast");
-        $("#newSearch").hide("fast");
-    }   // end page load */
-
+    }) //end myButton
 
 }) // end document ready
 
