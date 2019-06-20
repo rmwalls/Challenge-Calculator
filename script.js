4a6f244418335a1211085bb757db59f733709052
ZelleM <groovyrlm@gmail.com>
2019-06-19 21:33:55 -0400

    Trying to get data validation to work

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
 // need to 
 //      validate input, is there input, is it a positive number?, make sure it isn't text
 //      default number of people to split the bill by if not entered
-//      need to clear input fields
 //      
 
 //declare functions as page loads to make sure that the elements are shown and hidden
 $(document).ready(function () {    
     
-    //Declare needed variables, initiate numberSplittingCheck to 1
-    var billTotal;
+    //Declare needed variables, initiate billTotal to 0, numberSplittingCheck to 1
+    var billTotal=0;
     var tipPercentage;
     var tipAmount;
     var grandTotal;
     var numberSplittingCheck=1;
+	console.log("initial val of NSC is " + numberSplittingCheck);
     var amountEach;
     
     //on button click do the math and display the results
     const myButton = document.querySelector("#calculate"); 
 
     myButton.addEventListener('click', () => {    // calculate the numbers
-        billTotal=document.getElementById("billTotal-input").value;
+        billTotal=parseFloat(document.getElementById("billTotal-input").value).toFixed(2);
         tipPercentage=document.getElementById("tipPercentage-input").value;
         tipAmount=parseFloat((tipPercentage - 1) * billTotal).toFixed(2);
         grandTotal = parseFloat(billTotal * tipPercentage).toFixed(2);
         numberSplittingCheck=document.getElementById("numberSplittingCheck-input").value;
+	console.log("after click NSC is " + numberSplittingCheck);
         amountEach = parseFloat(grandTotal / numberSplittingCheck).toFixed(2);
-        validateData();
-        document.getElementById("form").reset(); //Clear form for next entry
+	console.log("amountEach is " + amountEach);
+        validateBillData();
+	validateNSC();
+        //document.getElementById("form").reset(); //Clear form for next entry
         //write back to the page
+	$("#totalBill").show();
+	$("#tipAmount").show();
+	$("#totalTotal").show();
+	$("#numberSplittingCheck").show();
+	$("#amountEach").show();
         document.querySelector("#totalBill").innerHTML="<br>" + "The Bill is $" + "<strong>" + billTotal + "</strong>";
         document.querySelector("#tipAmount").innerHTML="The Total Tip is $" + "<strong>" + tipAmount + "</strong>";
         document.querySelector("#totalTotal").innerHTML="Making the Grand Total " + "<strong>" + "$" + grandTotal + "</strong>";
@@ -36,19 +44,36 @@ $(document).ready(function () {
  
     }) //end myButton
 
-    function validateData(){
-        if (billTotal == "" || billTotal.includes("-")) { //if billtotal is blank or has a negative number
+	const resetButton = document.querySelector("#reset"); 
+	resetButton.addEventListener('click', () => {
+	$("#totalBill").hide();
+	$("#tipAmount").hide();
+	$("#totalTotal").hide();
+	$("#numberSplittingCheck").hide();
+	$("#amountEach").hide();
+	return;
+	}) // end resetButton
+
+    function validateBillData(){
+        if (billTotal == 0 || billTotal.includes("-")) { //if billtotal is zero or has a negative number
           alert("Please enter a positive number as the bill total");
+	  console.log("in the if for billdata");
           return false;
-        } else if (numberSplittingCheck == "" || numberSplittingCheck <= 0) { //default to 1 if blank or less than 1
-          numberSplittingCheck=1;
-          return;
-        } //end if/else
-    } // end validate function
+        } else {
+	  return true;
+	} //end if-else
+        } //end valBill
+
+	function validateNSC() {
+	if (numberSplittingCheck == "" || numberSplittingCheck <= 0) { //default to 1 if blank or less than 1
+        numberSplittingCheck=1;
+	console.log("in the if for NSC " + numberSplittingCheck);
+	} else {
+	return;
+	} //end if-else
+    	} // end valNSC
 
         var audio = new Audio('Cash Register Cha Ching-SoundBible.mp3'); // identify audio file
         $('#calculate').click( () => audio.play() ); // play it when button is clicked
 
 }) // end document ready
-
- 
